879aa3f1dea4a9ba07413e6c55e7728784e33c80
Andrey Zu <andrey.zu@ya.ru>
2021-05-28 00:28:39 +0300

    Add replace and random functions

--- script.js
+++ script.js
@@ -1,36 +1,76 @@
-const FS = require('fs');
+/**
+ * Settings
+ * string - add string to the end of the name.
+ * replace - replace forbidden chars.
+ * random - add random string to the end of the name.
+ * dir - directory with the files to rename.
+ * @type {{random: boolean, string: string, replace: boolean, dir: string, fs: module:fs}}
+ */
+const settings = {
+    string: 'test',
+    replace: true,
+    random: false,
+    dir: './files/',
+    fs: require('fs')
+};
 
-const DIR = './files/';
-const STRING = '_test';
+/**
+ * Add random string to the name.
+ * @param length {number}
+ * @returns {string}
+ */
+function randomString(length = 12) {
+    let result = [];
+    const CHARS = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    const CHAR_LENGTH = CHARS.length;
 
+    for (let i = 0; i < length; i++) {
+        result.push(CHARS.charAt(Math.floor(Math.random() * CHAR_LENGTH)));
+    }
 
-function readFileNames (FS, DIR) {
-  const fileNames = FS.readdirSync(DIR, ['utf-8', true]);
-
-  return fileNames.length > 0 ? fileNames : null;
+    return result.join('');
 }
 
+/**
+ * Replace forbidden chars in the name.
+ * @param name
+ * @returns {*}
+ */
+function replaceChars (name) {
+    return name
+        .replace(/[ ]/ig, '_')
+        .replace(/[х]/ig, 'x')
+        .replace(/[а-яё]/ig, '');
+}
 
-function renameFiles (fileNames, FS, DIR, STRING) {
-  if (!fileNames) {
-    return console.log('There are no files to rename.');
-  }
+function readFileNames (settings) {
+    const fileNames = settings.fs.readdirSync(settings.dir, ['utf-8', true]);
 
-  fileNames.forEach(file => {
-    const fileStrings = file.split('.');
-    const fileName = fileStrings[0];
-    const fileExtantion = fileStrings[1];
+    return fileNames.length > 0 ? fileNames : null;
+}
 
-    const newName = fileName + STRING + '.' + fileExtantion;
+function renameFiles (fileNames, settings) {
+    if (!fileNames) {
+        return console.log('There are no files to rename.');
+    }
 
-    FS.rename(DIR + file, './DONE/' + newName, err => {
-      if (err) {
-        throw console.error(err);
-        return;
-      }
+    fileNames.forEach(file => {
+        const fileStrings = file.split('.');
+        const fileName = fileStrings[0];
+        const fileExtantion = fileStrings[1];
+
+        const newName = (settings.replace ? replaceChars(fileName) : fileName)
+            + '_' + settings.string
+            + (settings.random ? '_' + randomString() : '')
+            + '.' + fileExtantion;
+
+        settings.fs.rename(settings.dir + file, './done/' + newName, err => {
+            if (err) {
+                throw new Error(`Could not rename file ${err}`);
+            }
+        });
     });
-  });
-  console.log('OK.');
+    console.log('OK.');
 }
 
-renameFiles (readFileNames (FS, DIR), FS, DIR, STRING);
+renameFiles (readFileNames (settings), settings);
