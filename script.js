2e8d0c059db47b24cb71342b837f6194556cff65
Andrey Zu <andrey.zu@ya.ru>
2021-06-03 23:57:34 +0300

    Added support for dots in names.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 /**
  * Settings
- * string - add string to the end of the name.
- * replace - replace forbidden chars.
- * random - add random string to the end of the name.
- * dir - directory with the files to rename.
+ * @property {string} string - add string to the end of the name.
+ * @property {boolean} replace - replace forbidden chars.
+ * @property {boolean} random - add random string to the end of the name.
+ * @property {string} dir - directory with the files to rename.
  * @type {{random: boolean, string: string, replace: boolean, dir: string, fs: module:fs}}
  */
 const settings = {
@@ -34,19 +34,19 @@ function randomString(length = 12) {
 /**
  * Replace forbidden chars in the name.
  * @param name
- * @returns {*}
+ * @returns {string}
  */
 function replaceChars (name) {
     return name
-        .replace(/[ ]/ig, '_')
+        .replace(/[ ()]/ig, '_')
         .replace(/[х]/ig, 'x')
-        .replace(/[а-яё]/ig, '');
+        .replace(/[^a-z_0-9-]/ig, '');
 }
 
 function readFileNames (settings) {
     const fileNames = settings.fs.readdirSync(settings.dir, ['utf-8', true]);
 
-    return fileNames.length > 0 ? fileNames : null;
+    return fileNames.length > 0 ? fileNames : false;
 }
 
 function renameFiles (fileNames, settings) {
@@ -56,15 +56,15 @@ function renameFiles (fileNames, settings) {
 
     fileNames.forEach(file => {
         const fileStrings = file.split('.');
-        const fileName = fileStrings[0];
-        const fileExtantion = fileStrings[1];
+        const FILE_NAME = fileStrings.length > 1 ? fileStrings.slice(0, fileStrings.length - 1).join('_') : fileStrings[0];
+        const FILE_EXTENSION = fileStrings.length > 1 ? fileStrings[fileStrings.length - 1] : false;
 
-        const newName = (settings.replace ? replaceChars(fileName) : fileName)
-            + '_' + settings.string
+        const NEW_NAME = (settings.replace ? replaceChars(FILE_NAME) : FILE_NAME)
+            + (settings.string.length > 0 ? '_' + settings.string : '')
             + (settings.random ? '_' + randomString() : '')
-            + '.' + fileExtantion;
+            + (FILE_EXTENSION ? '.' + FILE_EXTENSION : '');
 
-        settings.fs.rename(settings.dir + file, './done/' + newName, err => {
+        settings.fs.rename(settings.dir + file, './done/' + NEW_NAME, err => {
             if (err) {
                 throw new Error(`Could not rename file ${err}`);
             }
